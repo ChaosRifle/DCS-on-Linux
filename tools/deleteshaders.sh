4e9f73e38759ffcc56fe6881a30c27e9cb031730
ChaosRifle <DC3120@gmail.com>
2025-07-27 00:12:02 -0600

    Update deleteshaders.sh

--- tools/deleteshaders.sh
+++ tools/deleteshaders.sh
@@ -2,6 +2,15 @@
 # edit the following path to your DCS savedgames directory. 
 DCS_SAVEDGAMES="/run/media/$USER/SN850X 2TB/games/dcs-world/drive_c/users/$USER/Saved Games/DCS.openbeta"
 
+
+# documentation/explanation
+
+# deleteshaders.sh will delete your dcs shaders (*not* to be confused with shader cache on linux when launching any proton game).
+# ED recommends to do this every patch, however most users do not. If you see something weird or have horrible performance, try this.
+
+
+
+
 # ensure our scripts are never run as root
 if [ "$(id -u)" -eq 0 ]; then
     echo "Please do not run scripts off the internet as root"
