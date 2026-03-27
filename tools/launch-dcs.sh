b2e306f55308257260556ad35fc2d4f53ba7812f
Chaos <DC3120@gmail.com>
2026-03-27 04:02:43 -0600

    relabled srs launch arg to 's' from 'v', var safety pass, prelim VR launch support
    relabled srs launch arg to 's' from 'v'
    var safety pass
    prelim VR launch support, first iteration. Untested (i dont own vr)

--- tools/launch-dcs.sh
+++ tools/launch-dcs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ver='0.1.2'
+ver='0.1.3'
 
 
 ###################################################################################################
@@ -10,6 +10,10 @@ if [ "$(id -u)" -eq 0 ]; then
   exit 0
 fi
 
+
+###################################################################################################
+#main script
+###################################################################################################
 dir_cfg="/home/$USER/.config/dcs-on-linux/"
 cfg_dir_prefix="prefix.cfg"
 cfg_dir_srs_prefix="srs_prefix.cfg"
@@ -29,14 +33,14 @@ load_dcs_wine_config() { #in function so it can be modified by switches
       exit 0
     fi
     if [ -f "$dir_prefix/runners/$cfg_preferred_dir_wine" ]; then
-      dir_wine="$dir_prefix/runners/"$(cat "$dir_prefix/runners/$cfg_preferred_dir_wine")"/bin"
+      dir_wine="$dir_prefix/runners/$(cat "$dir_prefix/runners/$cfg_preferred_dir_wine")/bin"
     else
       echo "config file $cfg_preferred_dir_wine missing, exiting."
       exit 0
     fi
   fi
   echo "prefix: $dir_prefix"
-  echo "runner: "$(cat "$dir_prefix/runners/$cfg_preferred_dir_wine")""
+  echo "runner: $(cat "$dir_prefix/runners/$cfg_preferred_dir_wine")"
 
   export WINEPREFIX="$dir_prefix"
   export WINEDLLOVERRIDES='wbemprox=n' #;dbghelp=n'
@@ -47,7 +51,7 @@ load_dcs_wine_config() { #in function so it can be modified by switches
   export __GL_SHADER_DISK_CACHE="1"
   export __GL_SHADER_DISK_CACHE_PATH="$dir_prefix/cahce/gl"
   export DXVK_SHADER_CACHE_PATH="$dir_prefix/cache/dxvk"
-  export DXVK_STATE_CACHE=1
+  export DXVK_STATE_CACHE="1"
   export DXVK_STATE_CACHE_PATH="$dir_prefix/cache/dxvk"
   export WINEDEBUG='-all' # clean up terminal spam
 
@@ -70,14 +74,14 @@ launch_srs(){
       exit 0
     fi
     if [ -f "$dir_srs_prefix/runners/$cfg_preferred_dir_wine" ]; then
-      dir_srs_wine="$dir_srs_prefix/runners/"$(cat "$dir_srs_prefix/runners/$cfg_preferred_dir_wine")"/bin"
+      dir_srs_wine="$dir_srs_prefix/runners/$(cat "$dir_srs_prefix/runners/$cfg_preferred_dir_wine")/bin"
     else
       echo "config file $cfg_preferred_dir_wine missing, exiting."
       exit 0
     fi
   fi
   echo "srs prefix: $dir_srs_prefix"
-  echo "srs runner: "$(cat "$dir_srs_prefix/runners/$cfg_preferred_dir_wine")""
+  echo "srs runner: $(cat "$dir_srs_prefix/runners/$cfg_preferred_dir_wine")"
   cd "$dir_srs_prefix/drive_c/srs"
 
   export WINEPREFIX="$dir_srs_prefix"
@@ -88,16 +92,16 @@ launch_srs(){
   else
     "$dir_srs_wine/wine" "$dir_srs_prefix/drive_c/srs/SR-ClientRadio.exe"
   fi
-  cd $anchor_dir
+  cd "$anchor_dir"
 }
 
-if [ $# -eq 0 ]; then #default run
+if [ "$#" -eq 0 ]; then #default run
   echo "you are running v$ver of the launcher script."
-  $0 -van
+  "$0" -san
   exit 1
 else
-  while getopts "hadilnopruvw" arg; do #arg run
-    case $arg in
+  while getopts "hadilnoprsuvw" arg; do #arg run
+    case "$arg" in
       h) printf "DCS on Linux Launch Script
 execution: $0
 Other Commands:
@@ -111,30 +115,36 @@ Modifier-Type switches (must be placed before Run-type, may be 'consumed' on use
   [-w] run as wineWayland                             - EXPERIMENTAL!
 
 Run-Type switches (mutually exclusive, only the first will function unless otherwise noted):
-  [-v] launch SRS                                     - can be used with other Run-Types, if first. ex: [-vaon]
+  [-s] launch SRS                                     - can be used with other Run-Types, if first. ex: [-saon]
   [-l] run DCS with launcher enabled                  - only works if not disabled in options.lua / ingame settings
   [-n] run DCS with launcher disabled
+  [-v] run DCS with VR parameters
 
   [-r] repair DCS game files
   [-u] update DCS game files
-  [-i] run updater with arguments to install specific modules   - NOT IMPLEMENTED!
+  [-i] CLI install specified module                   - must provide the string (ED's system), ex: './launch-dcs.sh -i SYRIA_terrain'
 
 
 ";;
-      a) export DXVK_ASYNC='1' ;;
       d) echo 'NOT YET IMPLEMENTED, please edit the if statement in the script that says "$0 -n" to have the flag you want as default, by replacing the "n" with another arg, like "l", so it says "$0 -l"'; exit 0 ;; #changing the default run type is WIP - FIXME
-      i) echo "$arg is not implemented!" ;;
-      l) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS.exe" ;;
-      n) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS.exe" "--no-launcher" ;;
+
+      a) export DXVK_ASYNC='1' ;;
       o) use_hud='1' ;;
       p) echo "$arg is not implemented!" ;;
+      w) export DISPLAY= ; ;;
+
+      s) $(launch_srs) & ;; #run in subshell and continue execution
+      l) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS.exe" ;;
+      n) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS.exe" "--no-launcher" ;;
+      v) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS.exe" "--no-launcher --force_enable_VR --force_OpenXR";;
+
       r) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS_updater.exe" "repair" ;;
       u) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS_updater.exe" "update" ;;
-      v) $(launch_srs) & ;; #run in subshell and continue execution
-      w) export DISPLAY= ;;
+      i) load_dcs_wine_config; "$dir_wine/wine" "$dir_prefix/$dir_dcs/DCS_updater.exe" 'install' "$2" ;;
+
       ?) echo "error: option -$OPTARG is not implemented, use -h to see available swithes"; exit ;;
     esac
   done
 fi
 
-exit 1
+exit
