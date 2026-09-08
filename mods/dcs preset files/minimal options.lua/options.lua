85c98d60ee6b3d79ff3c3d53210448187f28464a
Chaos <REDACTED>
2026-09-08 10:03:14 -0600

    create minimalistic options.lua for fresh installs

---
+++ mods/dcs preset files/minimal options.lua/options.lua
@@ -0,0 +1,19 @@
+options = {
+	["VR"] = {
+	},
+	["difficulty"] = {
+	},
+	["format"] = 1,
+	["graphics"] = {
+	},
+	["miscellaneous"] = {
+		["push_starthere"] = false,
+	},
+	["plugins"] = {
+	},
+	["sound"] = {
+		["volume"] = 30,
+	},
+	["views"] = {
+	},
+}
